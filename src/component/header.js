0a2ac9537ceaa6745dd1b7778799269008288579
hhhhp52 <hhhhp52@gmail.com>
2024-07-16 08:38:50 +0800

    Update Project Detail Block [Add] 1. Project JSON file [Update] 1. Detail block swift show or hidden 2.Header and Footer CSS

--- src/component/header.js
+++ src/component/header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import '../home/home.css'
 import './header.css'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
