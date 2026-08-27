1f23d84be136b553fd97da41c8fac47e30166567
Schnee <hhhhp52@gmail.com>
2026-08-27 11:26:24 +0800

    Modernize resume site and migrate to Vite

--- src/index.js
+++ src/index.js
@@ -1,39 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-
-import HomePage from './home/home'
-import GamePage from "./game/game";
-import ProjectPage from "./project/project";
-
-
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <HomePage/>,
-    },
-    {
-        path: "/schnee",
-        element: <HomePage/>,
-    },
-    {
-        path: "/game",
-        element: <GamePage/>,
-    },
-    {
-        path: "/schnee/project",
-        element: <ProjectPage/>,
-    },
-], { basename: '/schnee' });
+import HomePage from './home/home.jsx'
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+    React.createElement(
+        React.StrictMode,
+        null,
+        React.createElement(HomePage)
+    )
 );
